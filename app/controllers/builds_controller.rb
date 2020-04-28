af7c615dba8a3202971cb08608a588d6dff42b6c
akihito kawasaki <kawasaki03040304@gmail.com>
2020-04-28 13:37:35 +0900

    gemfileの不要な記述削除

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -1,30 +1,27 @@
 class BuildsController < ApplicationController
   before_action :set_build, only: [:show, :edit, :update, :destroy]
 
-  # GET /builds
-  # GET /builds.json
+  
   def index
     @builds = Build.all
   end
 
-  # GET /builds/1
-  # GET /builds/1.json
+  
   def show
   end
 
-  # GET /builds/new
+  
   def new
     @build = Build.new
     @station = 2.times{@build.stations.build}
   end
 
-  # GET /builds/1/edit
+ 
   def edit
     @station = 1.times{@build.stations.build}
   end
 
-  # POST /builds
-  # POST /builds.json
+  
   def create
     @build = Build.new(build_params)
 
@@ -39,8 +36,7 @@ class BuildsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /builds/1
-  # PATCH/PUT /builds/1.json
+  
   def update
     respond_to do |format|
       if @build.update(build_params)
@@ -53,8 +49,7 @@ class BuildsController < ApplicationController
     end
   end
 
-  # DELETE /builds/1
-  # DELETE /builds/1.json
+ 
   def destroy
     @build.destroy
     respond_to do |format|
@@ -64,12 +59,12 @@ class BuildsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    
     def set_build
       @build = Build.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
+   
     def build_params
       params.require(:build).permit(:name, :rent, :address, :age, :remark, stations_attributes: [:route, :age, :property_id, :time, :id, :_destroy])
 
